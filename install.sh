bfe5cad81f8238fdd31b6f88ac2452f9ca2e7e5e
rosenrot00 <121129193+rosenrot00@users.noreply.github.com>
2024-12-19 09:56:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,12 +40,13 @@ fi
 
 # Copy the freedi.py module to the Klipper extras directory
 echo "Copying $MODULE_NAME to $KLIPPER_EXTRAS_DIR..."
-cp "$REPO_MODULE_PATH" "$KLIPPER_EXTRAS_DIR"
+#cp "$REPO_MODULE_PATH" "$KLIPPER_EXTRAS_DIR"
+ln -s "$REPO_MODULE_PATH" "$KLIPPER_EXTRAS_DIR"
 
 if [ $? -eq 0 ]; then
     echo "Successfully installed $MODULE_NAME to $KLIPPER_EXTRAS_DIR."
 else
-    echo "Error: Failed to copy $MODULE_NAME to $KLIPPER_EXTRAS_DIR."
+    echo "Error: Failed to symlink $MODULE_NAME to $KLIPPER_EXTRAS_DIR."
     exit 1
 fi
 
