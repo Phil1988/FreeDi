c70cf6ce61ea76507460694f61eba33ab2d25798
C0co <nopublic@mail.com>
2025-01-28 20:04:45 +0100

    gettin freedi_update.sh back to buizz

--- FreeDiLCD/freedi_update.sh
+++ FreeDiLCD/freedi_update.sh
@@ -9,6 +9,7 @@ echo "Starting the freedi update process..."
 USER_NAME=$(whoami)
 SERVICE="FreeDi.service"
 BKDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+HOMEDIR=$(dirname "$(dirname "${BKDIR}")")
 BKDIR=$(dirname "${BKDIR}") # remove the last part of the path
 FREEDI_LCD_DIR="${BKDIR}/FreeDiLCD"
 REPO_MODULE_DIR="${BKDIR}/klipper_module"
@@ -18,7 +19,7 @@ LCD_FIRMWARE_DIR="${BKDIR}/screen_firmwares"
 git -C ${BKDIR} update-index --assume-unchanged FreeDiLCD/freedi_update.sh
 
 # Varialbles for the klipper module
-KLIPPER_EXTRAS_DIR="$HOME/klipper/klippy/extras"
+KLIPPER_EXTRAS_DIR="$HOMEDIR/klipper/klippy/extras"
 MODULE_NAME="freedi.py"
 
 # Cleaning klipper repo for all v1.20 users
@@ -34,10 +35,8 @@ fi
 git sparse-checkout add helpers/
 git sparse-checkout add mainboard_and_toolhead_firmwares/
 
-
 # Exclude freedi.py from the Klipper repo as we introduce it and thus shouldn't be considered by the repo
-if ! grep -q "klippy/extras/${MODULE_NAME}" "${HOME}/klipper/.git/info/exclude"; then
-    echo "klippy/extras/${MODULE_NAME}" >> "${HOME}/klipper/.git/info/exclude"
+if ! grep -q "klippy/extras/${MODULE_NAME}" "${HOMEDIR}/klipper/.git/info/exclude"; then
+    echo "klippy/extras/${MODULE_NAME}" >> "${HOMEDIR}/klipper/.git/info/exclude"
 fi
 echo "Successfully installed $MODULE_NAME to $KLIPPER_EXTRAS_DIR."
-
